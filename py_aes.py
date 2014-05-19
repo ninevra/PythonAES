c84beaa25e8bdf38bd29e2f02adc962462661407
ninevra <ninevra@gmail.com>
2014-05-18 17:32:39 -0700

    Implement word addition. Implement word multiplication by hardcoding the appropriate pattern.

--- py_aes.py
+++ py_aes.py
@@ -37,3 +37,30 @@ def byte_mulx(b):
     True
     """
     return b << 1 if b & 128 == 0 else (b << 1) ^ _m
+
+def word_add(a, b):
+    """Returns the sum of four-byte words A and B by letting each word 
+    represent a polynomial of degree 3 with coefficients in GF(2^8). A and B 
+    should be sequences of bytes."""
+    return bytes(ai ^ bi for ai in a for bi in b)
+
+def word_mul(a, b):
+    """Returns the product of four-byte words A and B mod x^4 + 1, 
+    calculated by representing words [a0, a1, a2, a3] as polynomials 
+    a3*x^3 + a2*x^2 + a1*x + a0. A and B should be sequences of bytes. 
+
+    >>> word_mul(bytes.fromhex('02010103'), bytes.fromhex('0e090d0b'))
+    (1, 0, 0, 0)
+    >>> word_mul(bytes.fromhex('fd08ab00'), (1,0,0,0))
+    (253, 8, 171, 0)
+    >>> word_mul(bytes.fromhex('fd08ab00'), (0,0,0,0))
+    (0, 0, 0, 0)
+    """
+    return (byte_mul(a[0], b[0]) ^ byte_mul(a[3], b[1]) ^ 
+                byte_mul(a[2], b[2]) ^ byte_mul(a[1], b[3]),
+            byte_mul(a[1], b[0]) ^ byte_mul(a[0], b[1]) ^ 
+                byte_mul(a[3], b[2]) ^ byte_mul(a[2], b[3]),
+            byte_mul(a[2], b[0]) ^ byte_mul(a[1], b[1]) ^ 
+                byte_mul(a[0], b[2]) ^ byte_mul(a[3], b[3]),
+            byte_mul(a[3], b[0]) ^ byte_mul(a[2], b[1]) ^
+                byte_mul(a[1], b[2]) ^ byte_mul(a[0], b[3]))
